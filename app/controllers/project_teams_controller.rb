4a50f82a31738c7bbe9b95d016366ab766752fe8
Bhagya <st115637@ait.asia>
2015-03-24 22:05:55 +0700

    Logo added

---
+++ app/controllers/project_teams_controller.rb
@@ -0,0 +1,22 @@
+class ProjectTeamsController < ApplicationController
+  def index
+    @project = Project.find(params[:projectId])
+    @projectTeam = ProjectTeam.new
+  end
+
+  def create
+    @project = Project.find(params[:project_team][:project_id])
+    #@team = ProjectTeam.new(:user_id => project_params[:id],:project_id => params[:projectId])
+    @team = @project.project_teams.create(:user_id => params[:project_team][:user_id],:project_id => params[:project_team][:project_id])
+    @team.save
+    redirect_to project_path(@project)
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:id)
+  end
+  def projectTeam_params
+    params.require(:project_team).permit(:project_id,:user_id)
+  end
+end
